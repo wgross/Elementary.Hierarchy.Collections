e3cff8607e3f2c40f3409fb885c43d0caac6dfb7
Wolfgang Groß <github@text-mode.de>
2016-03-05 15:01:29 +0100

    Initial commit on ImmutableHierarchy

---
+++ Elementary.Hierarchy.Collections/IHierarchyNode.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Elementary.Hierarchy.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IHierarchyNode<TKey, TItem> :
+        // Support the public hierarchy traversal methods
+        IHasChildNodes<TItem>,
+        IHasIdentifiableChildNodes<TKey, TItem>,
+        IHasParentNode<TItem>
+        where TItem : IHierarchyNode<TKey, TItem>
+    {
+        TKey LocalPath { get; }
+
+        TItem AddChildNode(TItem newChild);
+
+        void ClearChildNodes();
+    }
+}
