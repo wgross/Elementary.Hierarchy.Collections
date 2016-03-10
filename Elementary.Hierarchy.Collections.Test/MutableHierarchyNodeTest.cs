dd1941caba4a4d169df98000d44051c599e2807f
Wolfgang Groß <bitbucket@text-mode.de>
2016-03-10 22:08:04 +0100

    Mutable node tested

---
+++ Elementary.Hierarchy.Collections.Test/MutableHierarchyNodeTest.cs
@@ -0,0 +1,190 @@
+﻿using Elementary.Hierarchy.Collections.Elementary.Hierarchy.Collections;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Elementary.Hierarchy.Collections.Test
+{
+    [TestFixture]
+    public class MutableHierarchyNodeTest
+    {
+        [Test]
+        public void MHN_TryGetValue_is_false_if_node_has_no_value()
+        {
+            // ARRANGE
+
+            var node = new MutableHierarchy<string, string>.Node("id");
+
+            // ACT
+
+            string value;
+            var result = node.TryGetValue(out value);
+
+            // ASSERT
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void MHN_TryGetValue_is_true_if_node_has_value()
+        {
+            // ARRANGE
+
+            var value = "value";
+            var node = new MutableHierarchy<string, string>.Node("id", value);
+
+            // ACT
+
+            string value_result;
+            var result = node.TryGetValue(out value_result);
+
+            // ASSERT
+
+            Assert.IsTrue(result);
+            Assert.AreSame(value, value_result);
+        }
+
+        [Test]
+        public void MHN_Setting_node_value_creates_a_node_node()
+        {
+            // ARRANGE
+
+            var node = new MutableHierarchy<string, string>.Node("id", "value");
+
+            // ACT
+
+            var result = node.SetValue("newValue");
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+            Assert.AreEqual("newValue", result.value);
+        }
+
+        [Test]
+        public void MHN_Setting_node_value_twice_doesnt_create_new_node()
+        {
+            // ARRANGE
+
+            string value = "value";
+            var node = new MutableHierarchy<string, string>.Node("id", value);
+
+            // ACT
+
+            var result = node.SetValue(value);
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+            Assert.AreSame(value, result.value);
+        }
+
+        [Test]
+        public void MHN_Adding_child_doesnt_create_new_node()
+        {
+            // ARRANGE
+
+            var childNode = new MutableHierarchy<string, string>.Node("id2", "value2");
+            var node = new MutableHierarchy<string, string>.Node("id", "value");
+
+            // ACT
+
+            var result = node.AddChildNode(childNode);
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+
+            // node contains child
+            Assert.AreEqual(1, result.ChildNodes.Count());
+            Assert.AreSame(childNode, result.ChildNodes.ElementAt(0));
+            Assert.AreEqual("value2", result.ChildNodes.ElementAt(0).value);
+        }
+
+        [Test]
+        public void MHN_Add_child_twice_doesnt_create_new_node()
+        {
+            // ARRANGE
+
+            var childNode = new MutableHierarchy<string, string>.Node("id2", "value2");
+            var node = new MutableHierarchy<string, string>.Node("id", "value");
+            node = node.AddChildNode(childNode);
+
+            // ACT
+
+            var result = node.AddChildNode(childNode);
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+
+            // node contains child twice.
+            Assert.AreSame(childNode, result.ChildNodes.ElementAt(0));
+            Assert.AreSame(childNode, result.ChildNodes.ElementAt(1));
+        }
+
+        [Test]
+        public void MHN_Set_child_node_exchanges_child_and_doesnt_create_new_node()
+        {
+            // ARRANGE
+
+            var childNode = new MutableHierarchy<string, string>.Node("child", "value2");
+            var node = new MutableHierarchy<string, string>.Node("id", "value", new[] { childNode });
+
+            // ACT
+
+            var childNode2 = new MutableHierarchy<string, string>.Node("child", "value");
+            var result = node.SetChildNode(childNode2);
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+
+            // new node contains the new child node
+            Assert.AreEqual(1, result.ChildNodes.Count());
+            Assert.AreSame(childNode2, result.ChildNodes.ElementAt(0));
+        }
+
+        [Test]
+        public void MHN_Unset_value_of_node_doesnt_create_new_node()
+        {
+            // ARRANGE
+
+            var node = new MutableHierarchy<string, string>.Node("id", "value", new[] {
+                new MutableHierarchy<string,string>.Node("child", "value2")
+            });
+
+            // ACT
+
+            var result = node.UnsetValue();
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+
+            // node has no value
+            Assert.IsFalse(result.HasValue);
+            Assert.AreEqual(1, result.ChildNodes.Count());
+            Assert.AreSame(node.ChildNodes.ElementAt(0), result.ChildNodes.ElementAt(0));
+        }
+
+        [Test]
+        public void MHN_Unset_value_twice_doesnt_create_new_node()
+        {
+            // ARRANGE
+
+            var node = new MutableHierarchy<string, string>.Node("id", "value", new[] {
+                new MutableHierarchy<string,string>.Node("child", "value2")
+            });
+
+            node = node.UnsetValue();
+
+            // ACT
+
+            var result = node.UnsetValue();
+
+            // ASSERT
+
+            Assert.AreSame(node, result);
+        }
+    }
+}
---
+++ Elementary.Hierarchy.Collections/MutableHierarchy.cs
@@ -0,0 +1,278 @@
+﻿namespace Elementary.Hierarchy.Collections
+{
+    namespace Elementary.Hierarchy.Collections
+    {
+        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+
+        /// <summary>
+        /// An immutable hierachy holds a set of value but never changes ists structure.
+        /// Any strcuture change like adding or removing a node produces a new hierarchy.
+        /// The data is copied wothe the nodes. If TValue is a reference type all hierechay reference the same data
+        /// If TValue is a value type thevalues are cpied with their nodes.
+        /// </summary>
+        /// <typeparam name="TKey">type of the indetifier of the stires data</typeparam>s
+        /// <typeparam name="TNode"></typeparam>
+        public class MutableHierarchy<TKey, TValue>
+        {
+            /// <summary>
+            /// Internal node class: holds a value and child nodes.
+            /// </summary>
+            public sealed class Node : IHasIdentifiableChildNodes<TKey, Node>
+            {
+                /// <summary>
+                /// Null value marker
+                /// </summary>
+                private static readonly object ValueNotSet = new object();
+
+                #region Construction and initialization of this instance
+
+                public Node(TKey id)
+                {
+                    this.id = id;
+                    this.value = ValueNotSet;
+                    this.childNodes = new Node[0];
+                }
+
+                public Node(TKey id, object value)
+                {
+                    this.id = id;
+                    this.value = value;
+                    this.childNodes = new Node[0];
+                }
+
+                public Node(TKey id, object value, IEnumerable<Node> childNodes)
+                {
+                    this.id = id;
+                    this.value = value;
+                    this.childNodes = childNodes.ToArray();
+                }
+
+                public readonly TKey id;
+
+                public object value = ValueNotSet;
+
+                private Node[] childNodes;
+
+                #endregion Construction and initialization of this instance
+
+                #region IHasChildNodes Members
+
+                public bool HasChildNodes => this.childNodes.Any();
+
+                public IEnumerable<Node> ChildNodes => this.childNodes;
+
+                #endregion IHasChildNodes Members
+
+                #region IHasIdentifiableChildNodes Members
+
+                public bool TryGetChildNode(TKey id, out Node childNode)
+                {
+                    childNode = this.childNodes.SingleOrDefault(n => EqualityComparer<TKey>.Default.Equals(n.id, id));
+                    return childNode != null;
+                }
+
+                #endregion IHasIdentifiableChildNodes Members
+
+                public bool HasValue => this.value != ValueNotSet;
+
+                public Node AddChildNode(Node newChildNode)
+                {
+                    // copy the existing children to a new array, and append the new one.
+                    Node[] newChildNodes = new Node[this.childNodes.Length + 1];
+                    Array.Copy(this.childNodes, newChildNodes, this.childNodes.Length);
+                    newChildNodes[this.childNodes.Length] = newChildNode;
+                    // set nw chile array instead of current child node array
+                    this.childNodes = newChildNodes;
+                    return this;
+                }
+
+                public Node SetChildNode(Node newChildNode)
+                {
+                    for (int i = 0; i < this.childNodes.Length; i++)
+                    {
+                        if (EqualityComparer<TKey>.Default.Equals(this.childNodes[i].id, newChildNode.id))
+                        {
+                            //substitute the existing child node with the new one.
+                            this.childNodes[i] = newChildNode;
+                            return this;
+                        }
+                    }
+                    throw new InvalidOperationException($"The node (id={newChildNode.id}) doesn't substutite any of the existing child nodes in (id={this.id})");
+                }
+
+                public Node SetValue(TValue value)
+                {
+                    this.value = value;
+                    return this;
+                }
+
+                /// <summary>
+                /// Gets value from node. If value is set treu is returned and the out parameter ist set.
+                /// </summary>
+                /// <param name="value"></param>
+                /// <returns>true if node has a value</returns>
+                public bool TryGetValue(out TValue value)
+                {
+                    value = default(TValue);
+                    if (!this.HasValue)
+                        return false;
+
+                    value = (TValue)this.value;
+                    return true;
+                }
+
+                /// <summary>
+                /// Unset ths value of this this node instance.
+                /// </summary>
+                /// <returns></returns>
+                public Node UnsetValue()
+                {
+                    this.value = ValueNotSet;
+                    return this;
+                }
+            }
+
+            #region Construction and initialization of this instance
+
+            public MutableHierarchy()
+                : this(new Node(default(TKey)))
+            {
+            }
+
+            private MutableHierarchy(Node rootNode)
+            {
+                this.rootNode = rootNode;
+            }
+
+            private readonly Node rootNode;
+
+            private MutableHierarchy<TKey, TValue> CreateIfRootHasChanged(Node newRoot)
+            {
+                if (object.ReferenceEquals(this.rootNode, newRoot))
+                    return this;
+
+                return new MutableHierarchy<TKey, TValue>(newRoot);
+            }
+
+            #endregion Construction and initialization of this instance
+
+            /// <summary>
+            /// Adds a value to the immutable hierachy at the specified position.
+            /// The result is a new ImmutableHiarachy contains the value. The
+            /// old one is unchanged.
+            /// If the value is equal to the value already stored at the position the hierachy remains unchanged.
+            /// </summary>
+            /// <param name="hierarchyPath">Specifies where to set the value</param>
+            /// <param name="value">the value to keep</param>
+            /// <returns>Am immutable hierach which contains the specified value</returns>
+            public MutableHierarchy<TKey, TValue> Add(HierarchyPath<TKey> hierarchyPath, TValue value)
+            {
+                // if the path has no items, tthe root node is changed
+                if (!hierarchyPath.Items.Any())
+                    return this.CreateIfRootHasChanged(this.rootNode.SetValue(value));
+
+                // make a snapshot of the path items for easier handling
+                var hierarchyPathItems = hierarchyPath.Items.ToArray();
+                var hierarchyPathItemsLength = hierarchyPathItems.Length;
+
+                // Create the new value node with the ngiven value and the leaf id.
+
+                Stack<Node> nodesAlongPath = new Stack<Node>();
+
+                var currentNode = this.rootNode;
+
+                // descend until the parent of the valueNode is reached
+                for (int currentHierarchyLevel = 0; currentHierarchyLevel < hierarchyPathItemsLength; currentHierarchyLevel++)
+                {
+                    Node nextNode = null;
+
+                    if (currentNode.TryGetChildNode(hierarchyPathItems[currentHierarchyLevel], out nextNode))
+                    {
+                        // child exists, just descend further
+                        nodesAlongPath.Push(currentNode);
+                    }
+                    else
+                    {
+                        // child nodes doesn't exist -> create new one
+                        if (currentHierarchyLevel < hierarchyPathItemsLength - 1)
+                        {
+                            // parent of new node isn't ready yet. Just another node.
+                            nextNode = new Node(id: hierarchyPathItems[currentHierarchyLevel]);
+                        }
+                        else
+                        {
+                            // this is the parent node of the value node.
+                            nextNode = new Node(id: hierarchyPathItems[currentHierarchyLevel], value: value);
+                        }
+
+                        nodesAlongPath.Push(currentNode.AddChildNode(nextNode));
+                    }
+                    currentNode = nextNode;
+                }
+
+                // new ascend agin to the root and clone new parnet node for the newly created child nodes.
+                while (nodesAlongPath.Any())
+                {
+                    // the next (parent node) get the current child node as a substitute.
+                    currentNode = nodesAlongPath.Peek().SetChildNode(currentNode);
+                    nodesAlongPath.Pop();
+                }
+
+                // this ist the new immutable hierachy root.
+                if (object.ReferenceEquals(this.rootNode, currentNode))
+                    return this;
+
+                return new MutableHierarchy<TKey, TValue>(currentNode);
+            }
+
+            /// <summary>
+            /// Retrieves the nodes value from the immutable hierarchy.
+            /// </summary>
+            /// <param name="hierarchyPath">path to the value</param>
+            /// <param name="value">found value</param>
+            /// <returns>zre, if value could be found, false otherwise</returns>
+            public bool TryGetValue(HierarchyPath<TKey> hierarchyPath, out TValue value)
+            {
+                value = default(TValue);
+                var valueNode = this.rootNode.DescendantAtOrDefault(hierarchyPath);
+                if (valueNode == null || !valueNode.HasValue)
+                    return false;
+
+                return valueNode.TryGetValue(out value);
+            }
+
+            /// <summary>
+            /// Removes the value from the specified node in hierarchy.
+            /// Value and nodes on under the specified nde remain unchanged
+            /// </summary>
+            /// <param name="hierarchyPath"></param>
+            /// <returns>true if value was removed</returns>
+            public MutableHierarchy<TKey, TValue> Remove(HierarchyPath<TKey> hierarchyPath)
+            {
+                // if the path has no items, the root node is changed
+                if (!hierarchyPath.Items.Any())
+                    return this.CreateIfRootHasChanged(this.rootNode.UnsetValue());
+
+                // now find the the value node and the path to reach it
+                Stack<Node> nodesAlongPath = new Stack<Node>(this.rootNode.DescentAlongPath(hierarchyPath));
+                if (nodesAlongPath.Count != hierarchyPath.Items.Count())
+                {
+                    // the value node doesn't exist: keep hierarchy as it is
+                    throw new KeyNotFoundException($"Could not find node '{hierarchyPath.Items.ElementAt(nodesAlongPath.Count)}' under '{HierarchyPath.Create(hierarchyPath.Items.Take(nodesAlongPath.Count)).ToString()}'");
+                }
+
+                // unset the value at the value node...
+                var currentNode = nodesAlongPath.Pop().UnsetValue();
+
+                // ... ascend again to the root and copy-on-change the ancestor nodes.
+                while (nodesAlongPath.Any())
+                    currentNode = nodesAlongPath.Pop().SetChildNode(currentNode);
+
+                // create new hierachy if root node has changed
+                return this.CreateIfRootHasChanged(this.rootNode.SetChildNode(currentNode));
+            }
+        }
+    }
+}
