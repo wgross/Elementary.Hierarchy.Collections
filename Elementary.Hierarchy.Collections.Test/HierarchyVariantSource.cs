982da57d96a97e9425c01f25feaab8e854c09e42
Wolfgang Groß <github@text-mode.de>
2016-07-21 18:38:52 +0200

    SetValueTest is now in one class for all hierachies.

---
+++ Elementary.Hierarchy.Collections.Test/HierarchyVariantSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elementary.Hierarchy.Collections.Test
+{
+    public class HierarchyVariantSource
+    {
+        public static IEnumerable WithouthDefaultValue
+        {
+            get
+            {
+                yield return new ImmutableHierarchy<string, string>();
+                yield return new MutableHierarchy<string, string>();
+            }
+        }
+    }
+}
